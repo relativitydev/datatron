ad4458988ec63259f63140850cf647fc59c5031d
flyingrabbit2600 <ivan.koretsky@relativity.com>
2018-01-09 19:33:56 -0600

    Added comments remove hard code for drive letting in DataTron.ps1

--- Source/Code/DataTron.ps1
+++ Source/Code/DataTron.ps1
@@ -1,5 +1,15 @@
 ﻿set-location /DataTron
 
+#Specify the drive letter where the service and java will be installed.
+
+$driveLetter = 'c'
+
+#Specity each node name to install along with the node type, for example:  ,("MyClientNode01" , "Client")
+#Begin each line with a comma, use double quotes to contain each value, separte the values with a comma.  
+
+#Add or remove lines as needed.  
+
+
 $nodes = @(
 ,("MasterNode1" , "Master")
 ,("DataNode1" , "Data")
@@ -9,6 +19,6 @@ $nodes = @(
 )
 
 foreach($node in $nodes) {
-$args = "-file .\Run-DataTron.ps1", "-driveLetter c", ("-machineName " + $node.get(0)), ("-nodeType " + $node.get(1))
+$args = "-file .\Run-DataTron.ps1", "-driveLetter $driveLetter", ("-machineName " + $node.get(0)), ("-nodeType " + $node.get(1))
 start-process powershell -ArgumentList $args 
 }
