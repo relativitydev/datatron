5b6bef2a9c1b87827d71ac1df94f9c9a85e122f0
Relativity ServiceAccount <ivan.koretsky@relativity.com>
2018-03-05 01:47:02 +0000

    Copy Folders protected.

--- Source/Code/DataTron/DataTron/Form2.cs
+++ Source/Code/DataTron/DataTron/Form2.cs
@@ -31,44 +31,50 @@ namespace DataTron
 
         private void btnCopyPackage_Click(object sender, EventArgs e)
         {
-            FolderBrowserDialog DialogBox = new FolderBrowserDialog();
-            DialogBox.ShowDialog();
-            string installPath = DialogBox.SelectedPath;
-            this.installPath = installPath;
-
-
-            void Copy(string sourceDirectory, string targetDirectory)
+            if (!Directory.Exists("RelativityDataGrid"))
             {
-                DirectoryInfo diSource = new DirectoryInfo(sourceDirectory);
-                DirectoryInfo diTarget = new DirectoryInfo(targetDirectory);
-
-                CopyAll(diSource, diTarget);
+                MessageBox.Show("Please Copy the RelativityDataGrid Folder to the executable folder.", "RelativityDataGrid Package not found!");
             }
-
-            void CopyAll(DirectoryInfo source, DirectoryInfo target)
+            else
             {
-                Directory.CreateDirectory(target.FullName);
+                FolderBrowserDialog DialogBox = new FolderBrowserDialog();
+                DialogBox.ShowDialog();
+                string installPath = DialogBox.SelectedPath;
+                this.installPath = installPath;
 
-                // Copy each file into the new directory.
-                foreach (FileInfo fi in source.GetFiles())
+
+                void Copy(string sourceDirectory, string targetDirectory)
                 {
-                    Console.WriteLine(@"Copying {0}\{1}", target.FullName, fi.Name);
-                    fi.CopyTo(Path.Combine(target.FullName, fi.Name), true);
+                    DirectoryInfo diSource = new DirectoryInfo(sourceDirectory);
+                    DirectoryInfo diTarget = new DirectoryInfo(targetDirectory);
+
+                    CopyAll(diSource, diTarget);
                 }
 
-                // Copy each subdirectory using recursion.
-                foreach (DirectoryInfo diSourceSubDir in source.GetDirectories())
+                void CopyAll(DirectoryInfo source, DirectoryInfo target)
                 {
-                    DirectoryInfo nextTargetSubDir =
-                        target.CreateSubdirectory(diSourceSubDir.Name);
-                    CopyAll(diSourceSubDir, nextTargetSubDir);
+                    Directory.CreateDirectory(target.FullName);
+
+                    // Copy each file into the new directory.
+                    foreach (FileInfo fi in source.GetFiles())
+                    {
+                        Console.WriteLine(@"Copying {0}\{1}", target.FullName, fi.Name);
+                        fi.CopyTo(Path.Combine(target.FullName, fi.Name), true);
+                    }
+
+                    // Copy each subdirectory using recursion.
+                    foreach (DirectoryInfo diSourceSubDir in source.GetDirectories())
+                    {
+                        DirectoryInfo nextTargetSubDir =
+                            target.CreateSubdirectory(diSourceSubDir.Name);
+                        CopyAll(diSourceSubDir, nextTargetSubDir);
+                    }
                 }
-            }
 
-            Copy("RelativityDataGrid", installPath + @"\RelativityDataGrid");
+                Copy("RelativityDataGrid", installPath + @"\RelativityDataGrid");
 
-
-            MessageBox.Show("Created the package at " + installPath);
+                MessageBox.Show("Created the package at " + installPath);
+            }  
         }
 
         private void btnSetJavaHeap_Click(object sender, EventArgs e)
