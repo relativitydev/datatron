381ae5c34c87d9780b8ef3d45aaa10e79a570192
Relativity ServiceAccount <ivan.koretsky@relativity.com>
2018-03-10 05:03:52 +0000

    Fixed blank memory size.

--- Source/Code/DataTron/DataTron/KService.cs
+++ Source/Code/DataTron/DataTron/KService.cs
@@ -140,8 +140,8 @@ goto:eof
 )
 
 :foundJVM
-if ""%ES_MIN_MEM%"" == """" set ES_MIN_MEM = {heap}g
-if ""%ES_MAX_MEM%"" == """" set ES_MAX_MEM = {heap}g
+if ""%ES_MIN_MEM%"" == """" set ES_MIN_MEM={heap}g
+if ""%ES_MAX_MEM%"" == """" set ES_MAX_MEM={heap}g
 
 if NOT ""%ES_HEAP_SIZE%"" == """" set ES_MIN_MEM=%ES_HEAP_SIZE%
 if NOT ""%ES_HEAP_SIZE%"" == """" set ES_MAX_MEM=%ES_HEAP_SIZE%
